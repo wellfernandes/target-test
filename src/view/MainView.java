bcd1a70e7e7b0694e237e36e3c9433c8088cffa3
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-14 01:53:56 -0300

    added events to main view buttons

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
@@ -17,4 +18,20 @@ public class MainView {
 	@FXML
 	private Button btnEx5;
 
+	@FXML
+	void onClicked(ActionEvent event) {
+
+		if (event.getSource().equals(btnEx2)) {
+			System.out.println("exercise 2");
+		}
+		if (event.getSource().equals(btnEx3)) {
+			System.out.println("exercise 3");
+		}
+		if (event.getSource().equals(btnEx4)) {
+			System.out.println("exercise 4");
+		}
+		if (event.getSource().equals(btnEx5)) {
+			System.out.println("exercise 5");
+		}
+	}
 }
