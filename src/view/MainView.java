03bff81b19ec4b48de296392474bd9c4aa67fed4
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-14 01:07:42 -0300

    project and main view created

---
+++ src/view/MainView.java
@@ -0,0 +1,20 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainView {
+
+	@FXML
+	private Button btnEx2;
+
+	@FXML
+	private Button btnEx3;
+
+	@FXML
+	private Button btnEx4;
+
+	@FXML
+	private Button btnEx5;
+
+}
